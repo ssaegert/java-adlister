6932c3846cf800c7f1e0e2c322e830aad97fdad8
ssaegert <ssaegert@gmail.com>
2018-03-28 13:35:42 -0500

    Refactored Color Exercise

--- src/main/java/ColorServlet.java
+++ src/main/java/ColorServlet.java
@@ -7,24 +7,14 @@ import java.io.IOException;
 
 @WebServlet(name = "Color", urlPatterns = "/pickcolor")
 public class ColorServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-//        request.getParameter("UserName");
         response.sendRedirect("/pickcolor.jsp");
-//
     }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-
         request.setAttribute("color", request.getAttribute("color"));
         request.getRequestDispatcher("/color.jsp").forward(request, response);
-
-//        String UserName = "World";
-//
-//        if (request.getMethod().equalsIgnoreCase("post")) {
-//            UserName = request.getParameter("UserName");
-////            response.sendRedirect("/surprise.jsp");
-//            response.getWriter().println("Hello, " + UserName);
-//        }
     }
+
 }
